e7b843672801e3604ed85900126aa774667ea0db
elise <Elisenovaes10@gmail.com>
2023-04-15 15:52:36 -0300

    User alterations

---
+++ src/main/java/com/example/baseproject/model/User.java
@@ -0,0 +1,32 @@
+package com.example.baseproject.model;
+
+/*import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+*/
+
+import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+//@Data
+//@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class User {
+  //  @Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	
+	private String nome;
+
+	private String username;
+
+}
