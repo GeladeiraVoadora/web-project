02019264b7464cd88ae66f138baaded374e29d23
elise <Elisenovaes10@gmail.com>
2023-04-15 14:06:42 -0300

    Update Product.java

--- src/main/java/com/example/baseproject/model/Product.java
+++ src/main/java/com/example/baseproject/model/Product.java
@@ -1,13 +1,26 @@
 package com.example.baseproject.model;
 
-import javax.persistence.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Product {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	
+	private String nome;
 
 }
