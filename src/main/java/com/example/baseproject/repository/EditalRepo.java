cf51dae3109fa51efd77bd7914d4484fec9e7f19
elise <Elisenovaes10@gmail.com>
2023-04-16 12:14:43 -0300

    Update EditalRepo.java

--- src/main/java/com/example/baseproject/repository/EditalRepo.java
+++ src/main/java/com/example/baseproject/repository/EditalRepo.java
@@ -1,12 +1,19 @@
 package com.example.baseproject.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.example.baseproject.model.Edital;
 
 @Repository
-public interface EditalRepo extends JpaRepository<Edital, String> {
-    
+public interface EditalRepo extends CrudRepository<Edital, String> {
+
+    List<Edital> findByTitulo(String titulo);
+    List<Edital> findByDescricao (String descricao);
+    List<Edital> findByPrazo (int prazo);
+    List<Edital> findByRequisitos (String requisitos);
+
+
     
 }
