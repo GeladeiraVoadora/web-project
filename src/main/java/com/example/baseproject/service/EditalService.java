f584301b22870c75e0b967769ad018534d9e9a4f
elise <Elisenovaes10@gmail.com>
2023-04-16 00:22:41 -0300

    Create EditalService.java

---
+++ src/main/java/com/example/baseproject/service/EditalService.java
@@ -0,0 +1,22 @@
+package com.example.baseproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.baseproject.model.Edital;
+import com.example.baseproject.repository.EditalRepo;
+
+@Service
+public class EditalService {
+
+    @Autowired
+    private EditalRepo editalRepo;
+
+    public Edital criarEdital(Edital edital) {
+        edital.setCreatedAt(new Date()); //FIXME: de onde importar o Date
+        return editalRepo.save(edital);
+    }
+
+    //mais operações de CRUD
+    
+}
